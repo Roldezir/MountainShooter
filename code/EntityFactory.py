d37fa3bfe54937519ebcb30a56cf9292774a50d7
Roldezir <140123210+Roldezir@users.noreply.github.com>
2024-10-24 09:31:03 -0300

    Update EntityFactory.py
    Removendo comentários

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -15,19 +15,19 @@ class EntityFactory:
         match entity_name:
             case 'Level1Bg':
                 list_bg = []
-                for i in range(7):  # level1bg images number
+                for i in range(7):  
                     list_bg.append(Background(f'Level1Bg{i}', (0, 0)))
                     list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
             case 'Level2Bg':
                 list_bg = []
-                for i in range(5):  # level2bg images number
+                for i in range(5):  
                     list_bg.append(Background(f'Level2Bg{i}', (0, 0)))
                     list_bg.append(Background(f'Level2Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
             case 'Level3Bg':
                 list_bg = []
-                for i in range(5):  # level2bg images number
+                for i in range(5):  
                     list_bg.append(Background(f'Level3Bg{i}', (0, 0)))
                     list_bg.append(Background(f'Level3Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
